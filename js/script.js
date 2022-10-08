aea68b847f525bfa3aacc9d13af5a370dc67f9e9
AnaCascante <ana.cascante@stud.noroff.no>
2022-10-08 21:30:47 +0200

    still not finish!!!!

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Question 1  
+
+var pets = [
+  {
+    type: "cat",
+    age: 5.5,
+  },
+  {
+    type: "dog",
+    age: 3.8,
+  },
+  {
+    type: "parrot",
+    age: 4.0,
+  },
+];
+
+for (var i=0; i <pets.length; i++){
+    var age = pets[i].age; 
+    var type = pets[i]; 
+    if (age >=4){
+        console.log (type);
+    }
+}
+
+// Question 2 
+// Create a function with one argument. done
+
+function loggIn (name){
+    var loggIn = name;
+    console.log (name)
+    
+    if (name) {
+        return true; 
+    }
+    else {
+        return "Please pass a boolean value in";
+    } 
+
+    
+}
+    loggIn (""); 
+
+// Inside the function check the type of the value being passed in.
+
+// If it is a boolean value, log the value.
+
+// If it is not a boolean value, log the message “Please pass a boolean value in”.
+
+// Call the function and pass a value in.
+
+
+// Question 3 Write code that does the following: When the button in the HTML is clicked, change the colour of the h2 element to “blue” and change it to read “Updated subheading”        
+
+var button = document.querySelector ("button")
+button.onclick = function (){
+    document.h2 = "Updated subheading"
+   
+}
+
+
+
+var h2 = document.querySelector ("h2")
+
